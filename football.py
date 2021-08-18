3908c545b4947af047143f7b1f229bb0248bdc2d
Sergei <89084197+sergKnyaz@users.noreply.github.com>
2021-08-18 22:05:58 +0300

    добавил dif init

--- football.py
+++ football.py
@@ -2,3 +2,6 @@ class Desk():
     '''Класс отвечает за генерацию колоды и ее тасовку'''
     RANGS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     SUIT = ['c', 'd', 'h', 's']
+    def __init__(self,rang,suit):
+        self.rang=rang
+        self.suit=suit
